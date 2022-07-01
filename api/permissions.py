92897d049773c5c60e23dd91dc1ec1c4ef5fcd4e
*MlKw0O2qj8 <hungnt2@fabbi.io>
2022-07-01 18:37:54 +0700

    feat:permision

--- api/permissions.py
+++ api/permissions.py
@@ -8,3 +8,13 @@ class IsAdmin(permissions.BasePermission):
         if view.kwargs['is_admin']:
             return True
         return False
+
+
+class IsAuthor(permissions.BasePermission):
+    edit_methods = ("GET", "PATCH", "POST", "PUT")
+
+    def has_permission(self, request, view):
+        if view.kwargs['is_author']:
+            return True
+        return False
+
